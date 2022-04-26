8979bfc5b092ce17013394eb86feef9581e563bc
Jose Granada <jroberto.granada@gmail.com>
2022-04-25 22:13:54 -0500

    Ajuste de permisos al usuario valle-user de mysql!

--- config_mysql.sql
+++ config_mysql.sql
@@ -2,9 +2,7 @@
 CREATE USER 'valle-user'@'%' IDENTIFIED BY 'Inclusionvalle2022*';
 -- Create database: talentumehs_valle_magico
 CREATE DATABASE talentumehs_valle_magico;
-GRANT ALL PRIVILEGES ON talentumehs_valle_magico.* TO 'valle-user'@'%' IDENTIFIED BY 'Inclusionvalle2022*';
-FLUSH PRIVILEGES;
 -- Create database: talentumehs_valle_magico
 CREATE DATABASE talentumehs_valle_open_location;
-GRANT ALL PRIVILEGES ON talentumehs_valle_open_location.* TO 'valle-user'@'%' IDENTIFIED BY 'Inclusionvalle2022*';
+GRANT ALL PRIVILEGES ON *.* TO 'valle-user'@'%';
 FLUSH PRIVILEGES;
